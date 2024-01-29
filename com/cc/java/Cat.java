09de8554bb4740555377cc722410f84eebf106e2
Nekroid <philipleinung@aol.de>
2024-01-29 10:55:34 +0100

    Supercat/Cat/Tomcat raw

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,34 +1,22 @@
 package com.cc.java;
 
-public class Cat {
+public class Cat
+{
 
-  private String name;
-  private String furColor;
-  private int age; 
   private int counter;
 
-  public Cat(String name, String furColor, int age) {
-    this.name = name;
-    this.furColor = furColor;
-    this.age = age;
+  public Cat(String name, String furColor, int age)
+  {
+    
   }
 
-  public String getStringAttributes(String op) {
-    switch (op) {
-      case "#name":
-        return name;
-      case "#color":
-        return furColor;
-      default:
-        return "ERROR!";
-    }
-  }
-
-  public String getAge() {
+  public String getAge()
+  {
     return checkCompliance();
   }
 
-  private String checkCompliance(){
+  private String checkCompliance()
+    {
 
     counter++;
 
--- com/cc/java/Tomcat.java
+++ com/cc/java/Tomcat.java
@@ -2,28 +2,13 @@ package com.cc.java;
 
 public class Tomcat {
 
-  private String name;
-  private String furColor;
-  private int age; 
-
-  public Tomcat(String name, String furColor, int age) {
-    this.name = name;
-    this.furColor = furColor;
-    this.age = age;
+  public Tomcat(String name, String furColor, int age)
+  {
+    
   }
 
-  public String getStringAttributes(String op) {
-    switch (op) {
-      case "#name":
-        return name;
-      case "#color":
-        return furColor;
-      default:
-        return "ERROR!";
-    }
-  }
-
-  public String getAge() {
+  public String getAge()
+  {
     return String.valueOf(age);
   }
 
